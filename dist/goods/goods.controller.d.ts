1dfa0fc94ba47baa95f6dce116d179b36790c8dc
DmytroQIvanov <20IvanovDmitriy03@gmail.com>
2021-07-22 09:13:59 +0300

    make it better

--- dist/goods/goods.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { GoodsService } from './goods.service';
-export declare class GoodsController {
-    private readonly goodsService;
-    constructor(goodsService: GoodsService);
-    getGoods(): any;
-}
--- dist/goods/goods.service.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class GoodsService {
-    getGoods(): void;
-}
